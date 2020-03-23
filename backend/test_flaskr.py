c839410636d579e8a84f2849272a635bc7422f72
Daniel R. Neal <dan.r.neal@gmail.com>
2020-03-22 20:45:25 -0700

    test: Create test for getting categories

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -9,6 +9,36 @@ from flaskr import app
 from models import DB_DIALECT, DB_HOST, DB_PORT, setup_db
 
 
+class CategoryTestCase(unittest.TestCase):
+    """This class represents the test cases for the category endpoints
+
+    Attributes:
+        app: A flask app from the flaskr app
+        client: A test client for the flask app to while testing
+        db_name: A str representing the name of the test database
+        db_path: A str representing the location of the test database
+    """
+
+    def setUp(self):
+        self.app = app
+        self.client = self.app.test_client
+        self.db_name = 'trivia_test'
+        self.db_path = f'{DB_DIALECT}://{DB_HOST}:{DB_PORT}/{self.db_name}'
+        setup_db(self.app, self.db_path)
+
+    def tearDown(self):
+        """Executed after each test"""
+
+    def test_get_categories_success(self):
+        """Test successful retrieval of categories"""
+
+        response = self.client().get('/categories')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json.get('success'), True)
+        self.assertTrue(response.json.get('categories'))
+
+
 class QuestionTestCase(unittest.TestCase):
     """This class represents the test cases for the question endpoints
 
@@ -17,7 +47,6 @@ class QuestionTestCase(unittest.TestCase):
         client: A test client for the flask app to while testing
         db_name: A str representing the name of the test database
         db_path: A str representing the location of the test database
-        new_book: A dict representing a new book to use in tests
     """
 
     def setUp(self):
