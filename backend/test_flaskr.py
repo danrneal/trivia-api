8363a7cc02bd97542a8b8e3745dce35eb453e699
Daniel R. Neal <dan.r.neal@gmail.com>
2020-03-23 13:48:36 -0700

    test: Add tests for error handlers

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -5,7 +5,7 @@ Classes:
 """
 
 import unittest
-from flaskr import app
+from flaskr import app, QUESTIONS_PER_PAGE
 from models import DB_DIALECT, DB_HOST, DB_PORT, setup_db, Question
 
 
@@ -40,6 +40,17 @@ class QuestionTestCase(unittest.TestCase):
         self.assertTrue(response.json.get('total_questions'))
         self.assertIsNone(response.json.get('current_category_id'))
         self.assertTrue(response.json.get('categories'))
+
+    def test_get_questions_out_of_range_fail(self):
+        """Test failed question retrieval when page number is out of range"""
+
+        total_pages = -(-Question.query.count() // QUESTIONS_PER_PAGE)
+
+        response = self.client().get(f'questions?page={total_pages+1}')
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json.get('success'), False)
+        self.assertEqual(response.json.get('message'), 'Not Found')
 
     def test_search_questions_success(self):
         """Test successful search of questions"""
@@ -90,6 +101,15 @@ class QuestionTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json.get('success'), True)
 
+    def test_create_question_no_info_fail(self):
+        """Test failed question creation when info is missing"""
+
+        response = self.client().post('/questions')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json.get('success'), False)
+        self.assertEqual(response.json.get('message'), 'Bad Request')
+
     def test_delete_question_success(self):
         """Test successful deletion of question"""
 
@@ -99,6 +119,16 @@ class QuestionTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json.get('success'), True)
         self.assertEqual(response.json.get('deleted_question_id'), question_id)
+
+    def test_delete_question_out_of_range_fail(self):
+        """Test failed questions deletion when question does not exist"""
+
+        question_id = Question.query.order_by(Question.id.desc()).first().id
+        response = self.client().delete(f'/questions/{question_id+1}')
+
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(response.json.get('success'), False)
+        self.assertEqual(response.json.get('message'), 'Unprocessable Entity')
 
 
 class CategoryTestCase(unittest.TestCase):
@@ -141,6 +171,21 @@ class CategoryTestCase(unittest.TestCase):
         self.assertTrue(response.json.get('total_questions'))
         self.assertEqual(response.json.get('current_category_id'), 1)
         self.assertTrue(response.json.get('categories'))
+
+    def test_get_category_questions_out_of_range_fail(self):
+        """Test failed category question retrieval when page is out of range"""
+
+        total_pages = -(-Question.query.filter(
+            Question.category_id == 1
+        ).count() // QUESTIONS_PER_PAGE)
+
+        response = self.client().get(
+            f'/categories/1/questions?page={total_pages+1}'
+        )
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json.get('success'), False)
+        self.assertEqual(response.json.get('message'), 'Not Found')
 
 
 class QuizTestCase(unittest.TestCase):
@@ -212,6 +257,15 @@ class QuizTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json.get('success'), True)
         self.assertIsNone(response.json.get('question'))
+
+    def test_create_quiz_no_info_fail(self):
+        """Test failed quiz creation when info is missing"""
+
+        response = self.client().post('/quizzes')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json.get('success'), False)
+        self.assertEqual(response.json.get('message'), 'Bad Request')
 
 
 if __name__ == "__main__":
